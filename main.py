fc6b8cd9a4c47bb725e7e474a6dc2a19dbd280c9
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2025-03-16 23:48:31 +0800

    feat(main): add lifespan management and CORS middleware to FastAPI app

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+from contextlib import asynccontextmanager
+
+from databases import database
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from routers.employee import router as employee_router
 
 app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(employee_router, prefix="/api/v1/employee")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health-check")
