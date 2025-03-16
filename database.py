86994c4b691d6f8a00f84a9d0a5e79a075e33fce
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2025-03-16 23:46:06 +0800

    Add database.py file

---
+++ database.py
@@ -0,0 +1,28 @@
+import databases
+import sqlalchemy
+
+from config import config
+
+metadata = sqlalchemy.MetaData()
+
+employee = sqlalchemy.Table(
+    "employee",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("department", sqlalchemy.String),
+    sqlalchemy.Column("lottery_eligibility", sqlalchemy.String),
+    sqlalchemy.Column("employee_id", sqlalchemy.String),
+    sqlalchemy.Column("is_won", sqlalchemy.Boolean),
+    sqlalchemy.Column("is_donated", sqlalchemy.Boolean),
+)
+
+connect_args = {"check_same_thread": False} if "sqlite" in config.DATABASE_URL else {}
+engine = sqlalchemy.create_engine(config.DATABASE_URL, connect_args=connect_args)
+
+metadata.create_all(engine)
+
+db_args = {"min_size": 5, "max_size": 30} if "postgres" in config.DATABASE_URL else {}
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLLBACK, **db_args
+)
